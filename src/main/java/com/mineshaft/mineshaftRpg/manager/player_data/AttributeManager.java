cdf26b6fd7a4b3d3c64dbc59eb5fca91f92830bc
SF-24 <legonexoknights123@gmail.com>
2025-06-22 16:40:55 +0100

    Fix

--- src/main/java/com/mineshaft/mineshaftRpg/manager/player_data/AttributeManager.java
+++ src/main/java/com/mineshaft/mineshaftRpg/manager/player_data/AttributeManager.java
@@ -26,7 +26,7 @@ public class AttributeManager {
 
 
     public static int calculateAttributeModifier(Player player, String attributeName) {
-        return PlayerStatManager.calculateAttributeModifier(JsonPlayerBridge.getAttribute(player, attributeName));
+        return PlayerStatManager.getAttributeModifier(JsonPlayerBridge.getAttribute(player, attributeName));
     }
 
 }
